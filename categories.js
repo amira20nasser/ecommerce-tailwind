cdc0b9d19619b97803919309eb958211f6014a7e
amira_nasser <amira20nasser@gmail.com>
2026-03-23 18:29:38 +0200

    add categories page and update navigation links to include categories

---
+++ categories.js
@@ -0,0 +1,70 @@
+const container = document.getElementById("categories");
+const loading = document.getElementById("loading");
+
+async function fetchCategories() {
+    try {
+        const res = await fetch("https://dummyjson.com/products/category-list");
+        const categories = await res.json();
+
+        const categoriesWithImages = await Promise.all(
+            categories.map(async (category) => {
+                try {
+                    const res = await fetch(`https://dummyjson.com/products/category/${category}`);
+                    const data = await res.json();
+
+                    return {
+                        name: category,
+                        image: data.products?.[0]?.thumbnail || ""
+                    };
+                } catch {
+                    return {
+                        name: category,
+                        image: ""
+                    };
+                }
+            })
+        );
+
+        loading.style.display = "none";
+        container.classList.remove("hidden");
+
+        categoriesWithImages.forEach((cat) => {
+            const card = document.createElement("div");
+
+            card.className = `
+            bg-white rounded-2xl overflow-hidden shadow-sm
+            hover:shadow-lg transition cursor-pointer
+            flex flex-col
+          `;
+
+            card.innerHTML = `
+            <div class="h-28 bg-gray-100 flex items-center justify-center">
+              <img 
+                src="${cat.image}" 
+                alt="${cat.name}"
+                class="h-full w-full object-contain"
+              />
+            </div>
+
+            <div class="p-3 text-center">
+              <h2 class="text-sm font-semibold text-gray-800 capitalize">
+                ${cat.name}
+              </h2>
+            </div>
+          `;
+
+            card.onclick = () => {
+                console.log("Category clicked:", cat.name);
+                window.location.href = `/products.html?category=${cat.name}`;
+            };
+
+            container.appendChild(card);
+        });
+
+    } catch (err) {
+        loading.innerText = "Failed to load categories";
+        console.error(err);
+    }
+}
+
+fetchCategories();
